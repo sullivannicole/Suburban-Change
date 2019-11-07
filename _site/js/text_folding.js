c167e137e7e552804681e69dcb83d779dd035b5a
Nicole Sullivan <sullivannicole@uchicago.edu>
2019-11-07 14:13:40 -0500

    Update button js and CSS

--- _site/js/text_folding.js
+++ _site/js/text_folding.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     $(".fold").prepend("<button class=\"fold-btn\">+</button>");
     $(".fold").children("code").toggle();
     $(".fold-btn").on("click", function() {
-      if($(this).text() === "Fold") {
+      if($(this).text() === "-") {
         $(this).text("+");
       } else {
         $(this).text("-");
