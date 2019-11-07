4ef8c5c37d9e5884b2c71af6629c4d646a8cf198
Nicole Sullivan <sullivannicole@uchicago.edu>
2019-11-07 14:19:27 -0500

    Update js reference

---
+++ _site/docs/js/text_folding.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $(".fold").prepend("<button class=\"fold-btn\">+</button>");
+    $(".fold").children("code").toggle();
+    $(".fold-btn").on("click", function() {
+      if($(this).text() === "-") {
+        $(this).text("+");
+      } else {
+        $(this).text("-");
+      }
+      $(this).next("code").toggle("linear");
+    });
+  });
