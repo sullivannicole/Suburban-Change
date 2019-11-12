faad1c7ab0ad6633459b96d24f1d1b9222420d71
Nicole Sullivan <sullivannicole@uchicago.edu>
2019-11-12 12:41:47 -0600

    Flesh out type A with specific resources

--- docs/docs/docs/docs/js/code_folding.js
+++ docs/docs/docs/docs/js/code_folding.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-    $(".fold").prepend("<button class=\"fold-btn\">+</button>");
+    $(".fold").prepend("<button class=\"fold-btn\">Unfold</button>");
     $(".fold").children("code").toggle();
     $(".fold-btn").on("click", function() {
       if($(this).text() === "Fold") {
-        $(this).text("+");
+        $(this).text("Unfold");
       } else {
-        $(this).text("-");
+        $(this).text("Fold");
       }
       $(this).next("code").toggle("linear");
     });
